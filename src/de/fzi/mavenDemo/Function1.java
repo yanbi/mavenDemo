7d1c96521b59b16967dba52952530019b0e5ba37
yanbi <yan.bi@fzi.de>
2014-11-17 14:20:57 +0100

    add java classes and tests

---
+++ src/de/fzi/mavenDemo/Function1.java
@@ -0,0 +1,30 @@
+/**
+ * Project Name: mavenDemo <br/>
+ * Package Name: de.fzi.mavenDemo <br/>
+ * File Name: Function1.java <br/>
+ * Date: 17.11.201413:52:01 <br/>
+ * Copyright (c) 2014 <br/>
+ * Yan Bi, biyan@gmail.com <br/>
+ * All Rights Reserved. <br/>
+ *
+ */
+package de.fzi.mavenDemo;
+
+/**
+ * File Name: Function1.java <br/>
+ * Class Name: Function1 <br/>
+ * date: 17.11.201413:52:01 <br/>
+ * @Author: persephony <br/>
+ *
+ */
+public class Function1 {
+
+	/**
+	 * Constructor <br/>
+	 * Function1:<br/>
+	 */
+	public Function1() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ test/de/fzi/mavenDemo/test/Function1Test.java
@@ -0,0 +1,41 @@
+/**
+ * Project Name: mavenDemo <br/>
+ * Package Name: de.fzi.mavenDemo.test <br/>
+ * File Name: Function1Test.java <br/>
+ * Date: 17.11.201413:53:19 <br/>
+ * Copyright (c) 2014 <br/>
+ * Yan Bi, biyan@gmail.com <br/>
+ * All Rights Reserved. <br/>
+ *
+ */
+package de.fzi.mavenDemo.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * File Name: Function1Test.java <br/>
+ * Class Name: Function1Test <br/>
+ * date: 17.11.201413:53:19 <br/>
+ * @Author: persephony <br/>
+ *
+ */
+public class Function1Test {
+
+	/**
+	 * enclosing_method: setUp <br/>
+	 * enclosing_type: Function1Test <br/>
+	 * @return void <br/>
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
